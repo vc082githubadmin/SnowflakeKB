7e8aee7b476a1d154ccc5f0e256f93c8bf8d633a
DILIPKUMAR KURUP <115324701+vc082githubadmin@users.noreply.github.com>
2023-12-02 21:01:52 -0500

    Create AdministrationCommands.sql

---
+++ Commands/AdministrationCommands.sql
@@ -0,0 +1,19 @@
+Account Management:
+Create Account <Account Name>
+  ADMIN_Name = <admin name>
+  ADMIN_Password = <Admin Password>
+  FIRST_NAME = <First Name>
+  LAST_NAME = <Last Name>
+  EMAIL = <Email>
+  EDITION = <Standard / Enterprise / Business Critical / Virtual Private Warehouse>
+  REGION = <Region>;
+
+Commands:
+  SHOW ORGANIZATION ACCOUNTS; #Allows the user to view all accounts in an organization 
+  SHOW REGIONS; # List the regions available for an organization 
+
+
+Enable Cross Account Features 
+
+Monitoring Account Usage
+
